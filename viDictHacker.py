6f1554e8f872571c767cf0d03cd8d0fd967bf111
Keyboard_dancer <cjy_mail@foxmail.com>
2014-12-27 16:36:30 +0800

    add freqanalysis and vidicthacker

---
+++ viDictHacker.py
@@ -0,0 +1,28 @@
+__author__ = 'Aaron'
+
+import detectEnglish, vigenereCipher, pyperclip
+
+def hackvigenere(ciphertext):
+	words = open('dict.txt').readlines()
+	for word in words:
+		word = word.strip()
+		decrytedtext = vigenereCipher.decryptMessage(word,ciphertext)
+		if detectEnglish.isEnglish(decrytedtext,40):
+			print 'Possible encryption break:'
+			print 'key' + word + ':' + decrytedtext[:100]
+			print 'enter d for done ,or just press enter to continue'
+			response = raw_input('>')
+			if response.upper().startswith('D'):
+				return decrytedtext
+
+def main():
+	ciphertext = """Tzx isnz eccjxkg nfq lol mys bbqq I lxcz."""
+	hackedMessage = hackvigenere(ciphertext)
+
+	if hackedMessage != None:
+		print hackedMessage
+	else:
+		print 'failed'
+
+if __name__ == '__main__':
+	main()
